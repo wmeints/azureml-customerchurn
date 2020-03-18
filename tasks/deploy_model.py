60bf3f1fc116577ea5d7aa33d15a9c3a6531d497
Willem Meints <1550763+wmeints@users.noreply.github.com>
2020-03-18 18:29:10 +0100

    Add deployment script

--- tasks/deploy_model.py
+++ tasks/deploy_model.py
@@ -4,6 +4,7 @@ from azureml.core import Workspace
 from azureml.core.model import Model, InferenceConfig
 from azureml.core.webservice import AciWebservice, Webservice
 
+
 @click.command()
 @click.option("--name", help="The name of the webservice")
 @click.option("--model", help="The name of the model to deploy")
@@ -13,22 +14,24 @@ def main(name, model):
 
     root_folder = Path(__file__).parent.parent
 
-    deployment_config = AciWebservice.deploy_configuration(cpu_cores=1, memory_gb=1)
+    deployment_config = AciWebservice.deploy_configuration(
+        cpu_cores=1, memory_gb=1, enable_app_insights=True)
 
     inference_config = InferenceConfig(
-        entry_script='customerchurn/score.py', 
+        entry_script='customerchurn/score.py',
         source_directory=root_folder
     )
 
     webservice = Model.deploy(
-        workspace=workspace, 
-        name=name, 
-        models=[model], 
-        deployment_config=deployment_config, 
+        workspace=workspace,
+        name=name,
+        models=[model],
+        deployment_config=deployment_config,
         inference_config=inference_config
     )
 
     webservice.wait_for_deployment(show_output=True)
+
 
 if __name__ == '__main__':
     main()
